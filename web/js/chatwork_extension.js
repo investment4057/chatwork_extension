3051c6975d1c98cb18086ff53bd89c383b356f78
sa8a <jpkrllll@yahoo.co.jp>
2023-02-22 01:01:27 +0900

    test: フォームに入力された値をチャットワークのコンソールに出力 #10

--- web/js/chatwork_extension.js
+++ web/js/chatwork_extension.js
@@ -5,31 +5,32 @@ async function getCurrentTab() {
 }
 
 async function allOpenedChat(myId, accessToken, clientVer) {
+  const tab = await getCurrentTab();
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: openedItem,
+    args: [myId, accessToken, clientVer]
+  });
+}
+
+function openedItem(myId, accessToken, clientVer) {
   console.log(myId);
   console.log(accessToken);
   console.log(clientVer);
 
-  // const tab = await getCurrentTab();
+  // let selectElements = document.querySelectorAll("#RoomList ul li[role=listitem]");
+  // var d=new Date();
+  // var rid;
 
-  // await chrome.scripting.executeScript({
-  //   target: { tabId: tab.id },
-  //   func: openedItem
+  // selectElements.forEach(function(element, index) {
+  //   rid = element.dataset.rid;
+
+  //   getRead = element.querySelector('li'); // 未読チャット確認のセレクタ
+  //   if (getRead) {
+  //     if (!getRead.hasAttribute('data-testid')) { // TOがなければ既読する
+  //       $.getJSON('https://'+window.location.hostname+'/gateway.php?cmd=read&myid='+MYID+'&_v='+CLIENT_VER+'&_av=5&_t='+ACCESS_TOKEN+'&ln=ja&room_id='+rid+'&last_chhkkt_id='+$('._message[data-rid='+rid+']:last  ').data('mid')+'&_='+d.getDate());
+  //     }
+  //   }
   // });
-}
-
-function openedItem() {
-  let selectElements = document.querySelectorAll("#RoomList ul li[role=listitem]");
-  var d=new Date();
-  var rid;
-
-  selectElements.forEach(function(element, index) {
-    rid = element.dataset.rid;
-
-    getRead = element.querySelector('li'); // 未読チャット確認のセレクタ
-    if (getRead) {
-      if (!getRead.hasAttribute('data-testid')) { // TOがなければ既読する
-        $.getJSON('https://'+window.location.hostname+'/gateway.php?cmd=read&myid='+MYID+'&_v='+CLIENT_VER+'&_av=5&_t='+ACCESS_TOKEN+'&ln=ja&room_id='+rid+'&last_chhkkt_id='+$('._message[data-rid='+rid+']:last  ').data('mid')+'&_='+d.getDate());
-      }
-    }
-  });
 }
