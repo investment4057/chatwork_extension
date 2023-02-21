0a2672ae5769f22181740d0fab220c474203f4a0
sa8a <jpkrllll@yahoo.co.jp>
2023-02-22 00:01:42 +0900

    MYIDを入力するフォームのUIを作成 #10

--- lib/main.dart
+++ lib/main.dart
@@ -28,38 +28,75 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: const Color.fromRGBO(240, 55, 71, 1),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Center(
             child: FutureBuilder(
               initialData: 'initial data',
               future: getUrl(),
               builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  if (snapshot.data!.contains('https://www.chatwork.com')) {
-                    return Column(
+                // if (snapshot.hasData) {
+                // if (snapshot.data!.contains('https://www.chatwork.com')) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text('[TO]以外のメッセージを一括で既読'),
-                        const SizedBox(height: 20),
+                        const Text('MYIDを入力'),
+                        SizedBox(
+                          width: 160,
+                          child: TextField(
+                            enabled: true,
+                            decoration: InputDecoration(
+                              hintText: 'MYID',
+                              filled: true,
+                              fillColor: Colors.grey.shade200,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('保存する'),
+                    ),
+                    Divider(
+                      color: Colors.grey[300],
+                      thickness: 2,
+                      height: 80,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('[TO]以外を一括既読する'),
                         ElevatedButton(
                           onPressed: () => allOpenedChat(),
                           child: const Text('一括既読'),
                         ),
                       ],
-                    );
-                  } else {
-                    return const Text(
-                      'Chatworkを開いてお使いください😥',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    );
-                  }
-                } else {
-                  return const Text(
-                    'データの取得に失敗しました😥',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  );
-                }
+                    ),
+                  ],
+                );
+                // } else {
+                // return const Text(
+                //   'Chatworkを開いてお使いください😥',
+                //   style: TextStyle(fontWeight: FontWeight.bold),
+                // );
+                // }
+                // } else {
+                // return const Text(
+                //   'データの取得に失敗しました😥',
+                //   style: TextStyle(fontWeight: FontWeight.bold),
+                // );
+                // }
               },
             ),
           ),
